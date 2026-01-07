5442afb4b1afd07778df8453d737857e0fb92591
Eren Kolac <erenk4036@gmail.com>
2026-01-07 16:01:59 +0100

    Feature: Funktion eingefügt

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-maxNumber = 250
-aktuelleZahl = 2
-primeNumberTrue = False
-dateiname = "primeNumber.txt"
+def finde_primzahlen(limit, dateiname):
+    aktuelleZahl = 2
 
-while aktuelleZahl <= maxNumber:
-    primeNumberTrue = True
-    testTeiler = 2
+    while aktuelleZahl <= limit:
+        primeNumberTrue = True
+        testTeiler = 2
 
-    while testTeiler < aktuelleZahl:
-    
-        if aktuelleZahl % testTeiler == 0:
-            primeNumberTrue = False
+        while testTeiler < aktuelleZahl:
+            if aktuelleZahl % testTeiler == 0:
+                primeNumberTrue = False
+            
+            testTeiler += 1
+
+        if primeNumberTrue == True:
+            with open(dateiname, "a") as datei_object:
+                print(aktuelleZahl, file=datei_object)
         
-        testTeiler += 1
+        aktuelleZahl += 1
 
-    if primeNumberTrue == True:
-        with open(dateiname, "a") as datei_object:
-            print(aktuelleZahl, file=datei_object)
-    
-    aktuelleZahl += 1
+
+
+finde_primzahlen(250, "primeNumber.txt")
 
 
         
